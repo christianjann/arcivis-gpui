96507e2630304dd4588db0b485db5a6dda87e559
Christian Jann <christian.jann@ymail.com>
2025-12-29 10:18:42 +0100

    Update conversion script

--- doc/convert_webm.sh
+++ doc/convert_webm.sh
@@ -1,2 +1,2 @@
-ffmpeg -i screencast.webm -vf "fps=15,scale=640:-1:flags=lanczos,palettegen" palette.png
-ffmpeg -i screencast.webm -i palette.png -filter_complex "fps=2,scale=800:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=bayer" screencast.gif
+ffmpeg -i screencast.webm -vf "fps=15,scale=1280:-1:flags=lanczos,palettegen" palette.png
+ffmpeg -i screencast.webm -i palette.png -filter_complex "fps=2,scale=1280:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=bayer:bayer_scale=3" screencast.gif
